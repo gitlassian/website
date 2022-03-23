d3d96b6cfe5e3652383b0980a8534db4ae2ca62e
Gitlassian <twinkkyarik@gmail.com>
2022-03-23 11:15:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+//insta
+function hoverinsta(element) {
+    element.setAttribute('src', 'images/instadown.png');
+  }
+function unhoverinsta(element) {
+    element.setAttribute('src', 'images/instaup.png');
+  }
+
+//discord
+function hoverdiscord(element) {
+    element.setAttribute('src', 'images/discorddown.png');
+  }
+function unhoverdiscord(element) {
+    element.setAttribute('src', 'images/discordup.png');
+  }
+
+//github
+function hovergithub(element) {
+    element.setAttribute('src', 'images/githubdown.png');
+  }
+function unhovergithub(element) {
+    element.setAttribute('src', 'images/githubup.png');
+  }
+
+//yt
+function hoveryt(element) {
+    element.setAttribute('src', 'images/ytdown.png');
+  }
+function unhoveryt(element) {
+    element.setAttribute('src', 'images/ytup.png');
+  }
+
+//logo
+function hoverlogo(element) {
+  element.setAttribute('src', 'images/logodown.png');
+}
+function unhoverlogo(element) {
+  element.setAttribute('src', 'images/logoup.png');
+}
+
+//home
+function hoverhome(element) {
+  element.setAttribute('src', 'images/homedown.png');
+}
+function unhoverhome(element) {
+  element.setAttribute('src', 'images/homeup.png');
+}
+
+//about
+function hoverabout(element) {
+  element.setAttribute('src', 'images/aboutdown.png');
+}
+function unhoverabout(element) {
+  element.setAttribute('src', 'images/aboutup.png');
+}
+
+//tp
+function hovertp(element) {
+  element.setAttribute('src', 'images/tpdown.png');
+}
+function unhovertp(element) {
+  element.setAttribute('src', 'images/tpup.png');
+}
+
+//contacts
+function hovercontacts(element) {
+  element.setAttribute('src', 'images/contactsdown.png');
+}
+function unhovercontacts(element) {
+  element.setAttribute('src', 'images/contactsup.png');
+}
